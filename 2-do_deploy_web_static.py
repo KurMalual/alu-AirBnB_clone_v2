47775c21b6d923b94d5a8d6ff395558b93a64c67
KurMalual <k.malual@alustudent.com>
2023-10-15 19:32:41 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,42 +1,40 @@
 #!/usr/bin/python3
-"""Fabric script that distributes an archive to your web servers"""
-from fabric.api import env, put, run
-from os.path import exists
-
-<<<<<<< HEAD
-env.hosts = ["54.209.26.141", "18.215.182.32"]
-env.user = "ubuntu"
-env.key = "~/.ssh/id_rsa"
-=======
-import os
+""" Function that compress a folder """
 from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
+from fabric.api import *
+import shlex
+import os
 
 
-env.hosts = ['34.228.27.169', '54.88.249.173']
->>>>>>> e53ddc400784148de62dde8d5ef114793def52d4
+env.hosts = ['18.212.97.222', '54.196.244.90']
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """Function to distribute an archive to your web servers"""
-    if not exists(archive_path):
+    """ Deploys """
+    if not os.path.exists(archive_path):
         return False
     try:
-        file_name = archive_path.split("/")[-1]
-        name = file_name.split(".")[0]
-        path_name = "/data/web_static/releases/" + name
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}/".format(path_name))
-        run('tar -xzf /tmp/{} -C {}/'.format(file_name, path_name))
-        run("rm /tmp/{}".format(file_name))
-        run("mv {}/web_static/* {}".format(path_name, path_name))
-        run("rm -rf {}/web_static".format(path_name))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}/ /data/web_static/current'.format(path_name))
-        return True
-    except Exception:
-        return False
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
 
-# Run the script like this:
-# $ fab -f 2-do_deploy_web_static.py
-# do_deploy:archive_path=versions/file_name.tgz
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
+        return True
+    except:
+        return False
